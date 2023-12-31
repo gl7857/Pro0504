f901a4d7a4b653d66eb91e494cd516c5b1595176
gl7857 <gl7857@bs.amalnet.k12.il>
2023-12-31 17:09:02 +0200

    Pro0504

--- app/src/main/java/com/example/pro0504/MainActivity.java
+++ app/src/main/java/com/example/pro0504/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         btn = findViewById(R.id.btn);
     }
 
+
     public void clicked(View view) {
         clicksNumber++;
         txView.setText("This is a click number:"+clicksNumber);
