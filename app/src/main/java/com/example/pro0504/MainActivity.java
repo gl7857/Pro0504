a6020d147fbf6bce3ab9b4ca131e648ac4472172
gl7857 <gl7857@bs.amalnet.k12.il>
2023-12-31 17:07:59 +0200

    pro0504

---
+++ app/src/main/java/com/example/pro0504/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.pro0504;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+    TextView txView;
+    int clicksNumber=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txView = findViewById(R.id.txView);
+        btn = findViewById(R.id.btn);
+    }
+
+    public void clicked(View view) {
+        clicksNumber++;
+        txView.setText("This is a click number:"+clicksNumber);
+        if (clicksNumber%7==0){
+            txView.setText("BOOM!");
+        }
+    }
+}
